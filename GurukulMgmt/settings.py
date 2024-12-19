7c4d9b8d475e31063743d8776754dcf054d052d5
sriram1990usa <60535385+sriram1990usa@users.noreply.github.com>
2024-12-19 21:17:38 +0530

    Update settings.py

--- GurukulMgmt/settings.py
+++ GurukulMgmt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n@z6b^56u6zbby-f7*am8)#np77t%p-zb67p^c66+qwo*-z0v*'
+SECRET_KEY =  os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
